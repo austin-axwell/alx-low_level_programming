f1e29974ba4d7c76adfc8e2b7a82d0395283b8e5
Austin Onyango <austintyg254@gmail.com>
2023-10-28 02:20:01 -0700

    0x08-recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 /**
-* _print_rev_recursion - function that prints a string in reverse.
-*@s: parameter string
-*
-*/
+ * _print_rev_recursion - Write a function that prints a string in reverse.
+ * @s: This is my entry
+ *
+ */
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
